ac5e0120bb2b8a7599197b93d4a0acb2157f211f
May Yi Aung <saunghninsi91@gmail.com>
2024-05-01 14:07:57 +0400

    remove secret key

--- aiems_test/settings.py
+++ aiems_test/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gm91okrp%0phg@u72lh+q4ga)eg291k7bdr3w=wh!mniz=j8rn'
+
+SECRET_KEY=""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
